8ff62e56bdc25cc04e9a08a8aa99c88bf94cd0ce
fantaKourouma~ <saratou123@gmail.com>
2024-11-20 11:26:03 -0500

    first commit

---
+++ src/TreesetExamplethree.java
@@ -0,0 +1,22 @@
+import java.util.TreeSet;
+
+public class TreesetExamplethree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(6);
+        System.out.println("TreeSet: " + numbers);
+
+        // Using the first() method
+        int first = numbers.first();
+        System.out.println("First Number: " + first);
+
+        // Using the last() method
+        int last = numbers.last();
+        System.out.println("Last Number: " + last);
+	}
+
+}
